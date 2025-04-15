eeb39266885cdc502563854fe46da91fc2e49f64
seeun214 <seeunjin22@naver.com>
2025-04-16 00:58:16 +0900

    chore: TailwindCSS 적용, FSD 기본 구조 설정

---
+++ apps/web/src/pages/board/ui/postList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const PostList = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h2 className="text-3xl font-bold mb-6">게시판</h2>
+    </div>
+  );
+};
