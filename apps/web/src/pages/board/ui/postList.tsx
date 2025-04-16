c1f0cba7a57e6236d364553f8546c4bcb1b361f8
seeun214 <seeunjin22@naver.com>
2025-04-16 14:54:40 +0900

    feat: header, footerNav 생성

--- apps/web/src/pages/board/ui/postList.tsx
+++ apps/web/src/pages/board/ui/postList.tsx
@@ -1,9 +1,16 @@
+import { FooterNav } from "@/widgets/footerNav";
+import { Header } from "@/widgets/header";
 import React from "react";
 
 export const PostList = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-3xl font-bold mb-6">게시판</h2>
+    <div className="container">
+      <div className="p-4">
+        <Header />
+        <h2 className="text-3xl font-bold mb-6">게시판</h2>
+      </div>
+
+      <FooterNav />
     </div>
   );
 };
